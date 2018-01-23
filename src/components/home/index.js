b4beac67a6455f1f31a51d9d774393f525d5020c
Matthew Greenberg <matt@Matthews-MacBook-Pro.local>
2018-01-23 12:26:56 -0600

    add smooth scroll

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import Header from '../header/header';
 import About from '../about/about';
 import Work from '../work/work';
 import Contact from '../contact/contact';
-
-
+import * as Scroll from 'react-scroll';
+import { Link, Element, animateScroll as scroll, scroller } from 'react-scroll';
 import './home.css'
 import {
   increment,
@@ -18,15 +17,50 @@ import {
 import purpleTriangle from './images/purple-triangle.png';
 
 class Home extends React.Component {
+  scrollToBottom = () => {
+    scroll.scrollToBottom({duration: 1500});
+  }
+  scrollTowork = () => {
+    scroller.scrollTo('work', {
+      duration: 1000,
+      delay: 100,
+      smooth: true,
+      containerId: 'work',
+  })
+  }
   render() {
     return (
       <div>
-        <script src="https://rawgithub.com/soulwire/sketch.js/master/js/sketch.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
         <div className='home' >
         <div id="container"></div>
-          <Header />
+        <header className='header'>
+          <Link 
+            className='header__link'
+            duration={500}
+            delay={100}
+            smooth={true}
+            to='work'
+          >
+            Work
+          </Link>
+          <Link 
+            className='header__link'
+            duration={750}
+            delay={100}
+            smooth={true}
+            to='about'
+          >
+            About
+          </Link>
+          <a 
+            className='header__link'
+            onClick={this.scrollToBottom}
+          >
+            Contact
+          </a>
+        </header>
           <div className='home__header--wrapper'>
             <h1 className='home__header'>Matt Greenberg</h1>
             <h2 className='home__sub-header'>Creative Developer</h2>
@@ -36,8 +70,12 @@ class Home extends React.Component {
             />
           </div>
         </div>
-        <Work />
-        <About />
+        <Element name='work'>
+          <Work />
+        </Element>
+        <Element name='about'>
+          <About />
+        </Element>
         <Contact />
       </div>
     );
